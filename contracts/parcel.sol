eee11d5d3ab3c75bb364be5528f60fd7866bfc4a
odyslam <odyslam@gmail.com>
2021-07-31 12:20:22 +0300

    parcel.sol | make it a wrapper erc721 contract

--- contracts/parcel.sol
+++ contracts/parcel.sol
@@ -1,40 +1,20 @@
-pragma solidity >=0.8.0 <0.9.0
+// SPDX-License-Identifier: MIT
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+pragma solidity ^0.8.0;
 
+import "./ERC721Tradable.sol";
 
-contract cityDaoParcel is ERC1155, Ownable {
-  uint256 private _currentTokenId = 0;
-  storage int parcelId = 0;
-  mapping(uint256 => uint256) public tokenSupply;
-  constructor(string memory _uri, string memory _name, string memory _symbol) ERC1155(https://parcels.citydao/api/parcels/{id}.json) 
-  {
-    name = _name;
-    symbol = _symbol;
-    Ownable.initialize(msg.sender);
-  }
-  struct Parcel {
-  uint256 id;
-  bytes32 lat;
-  bytes32 long;
-  uint16 leaseLength;
-  string metadataLocationHash;
-  // locationHash = IPFS://<hash> or AR://<hash>
-  }
-  mapping(uint256 => Parcel) public parcelOwners;
-  function mintParcel (
-    address _toAddress,
-    uint256 _quantity
-    ) external onlyOwner returns(uint256) { 
-    parcelId = parcelId + 1;
-    Parcel parcel = Parcel(parcelId, _lat, _long, _leaseLength, _metadataLocationHash)
-    _mint( _toAddress, parcelId);
-    parcelOwners[_toAddress] = parcelId;
-    parcels
-    return parcelId;
-   }
+/// @author Cityday team
+/// @title A simple ERC721 token that represents land parcels
 
-   function getParcelOwner (address parcelOwner) returns(
-
+contract Parcel is ERC721Parcel {
+  constructor(address _proxyRegistryAddress)
+    ERC721Tradable("CityDAO land parcels", "CITY", _proxyRegistryAddress)
+    {}
+    function baseTokenURI() override public pure returns (string memory){
+      return "https://parcels.cityDAO.com/api/parcels/";
+    }
+    function contractURI() public pure returns (string memory) {
+      return "https://parcels.cityDAO.com/contracts/ERC721-parcels";
+    }
+}
